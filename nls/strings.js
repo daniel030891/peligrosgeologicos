cdc0c3cf2f35d0d00278cbe3f4cc78b39333f394
daniel030891 <daniel030891@gmail.com>
2017-11-10 16:50:27 -0500

    Widget Peligros Geologicos v 0.1.0

---
+++ nls/strings.js
@@ -0,0 +1,19 @@
+define({
+  root: {
+    widgetTitle: 'hazards',
+    description: 'Identificación de peligros por susceptibilidad  y mov. en masa',
+    abstract: 'Determina características de un espacio en función al grado de susceptibilidad de mov. en masa e inundaciones por erosión',
+    hazard: 'Susceptibilidad y Mov. en masa',
+    floods: 'Inundación por erosión',
+    draw: 'Graficar',
+    feature: 'Agregar entidad',
+    placeholderMenu: 'Seleccione variable',
+    point: 'Graficar punto',
+    polyline: 'Graficar línea',
+    polygon: 'Graficar polígono',
+    erase: 'Eliminar gráficos',
+    lblSelectFile: 'Seleccionar archivo (.zip)'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});
